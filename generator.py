f1b856d31bdfc3c523f5349fdb670688399aa40a
myusername <myemail>
2022-05-10 21:38:43 +0530

    commited practiced python files

---
+++ generator.py
@@ -0,0 +1,22 @@
+#generator
+def mygen():
+    yield 'a'
+    yield 'b'
+
+g=mygen()
+print(type(g))
+
+
+
+
+
+#generate values oneby one dynamically
+def countdown(num):
+    print("Starrt conuntdown")
+    while(num>0):
+        yield num
+        num=num-1
+print(countdown(10))
+values=countdown(10)
+for x in values:
+    print(x)
